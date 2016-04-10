76cf29b71145ccf4e32221aec007f37756efee10
alex.fan <alex.fan@watchguard.com>
2016-04-10 22:30:01 +0800

    Added LoginActivity and SessionManager

--- app/src/main/java/com/skyuma/myfragment/MainActivity.java
+++ app/src/main/java/com/skyuma/myfragment/MainActivity.java
@@ -1,22 +1,26 @@
 package com.skyuma.myfragment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.widget.RadioGroup;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager;
+    private SessionManager session;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
 
         fragmentManager = getSupportFragmentManager();
         RadioGroup radioGroup = (RadioGroup) findViewById(R.id.rg_tab);
@@ -31,7 +35,16 @@ public class MainActivity extends FragmentActivity {
                 }
             }
         });
-
         findViewById(R.id.rb_home).performClick();
+
+        session = new SessionManager(getApplicationContext());
+        if(!session.isLoggedIn()){
+            // user is not logged in redirect him to Login Activity
+            Intent i = new Intent(this, LoginActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(i);
+            //finish();
+        }
     }
 }
